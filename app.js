88e43c6ea38a5c8ebea07271d0c7085bc5541cbb
Bhavuk Dhawan <devdhawan222@gmail.com>
2020-07-03 16:19:58 +0530

    repair some files

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const app =  express();
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-const id = "3a267299";
-const key = "2beec82d419c9979ec5edde927e27d3f";
+// const id = "3a267299";
+// const key = "2beec82d419c9979ec5edde927e27d3f";
 
 
 
-app.post("/recipe",(req,res) =>{
-   var url="https://api.edamam.com/search?q=chicken&app_id="+id+"&app_key="+ key
+// app.post("/recipe",(req,res) =>{
+//    var url="https://api.edamam.com/search?q=chicken&app_id="+id+"&app_key="+ key
    
 
-      request(url,function(error,response ,body)
-      {
-         console.log(body.hits[1].recipe.ingredientLines);
-      });
-   });
+//       request(url,function(error,response ,body)
+//       {
+//          console.log(body.hits[1].recipe.ingredientLines);
+//       });
+//    });
 app.get("/",(req,res)=>
 {
    res.sendFile(__dirname+"/index.html");
